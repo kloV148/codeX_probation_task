d243b4fa4d307bbc1f631ee9d2054be24723ff95
IvanS <mainpostskvorcov@gmail.com>
2023-10-02 01:01:35 +0300

    added note details and note update

--- example_project/simple_app/urls.py
+++ example_project/simple_app/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, NotesListView, NoteCreateView
+from .views import (
+    NotesIndexView,
+    NotesListView,
+    NoteCreateView,
+    NoteUpdateView,
+    NoteDetailView,
+)
 
 app_name = "simple_app"
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", NotesIndexView.as_view(), name="index"),
     path("list/", NotesListView.as_view(), name="notes_list"),
+    path("list/<int:pk>/", NoteDetailView.as_view(), name="note_detail"),
+    path("list/<int:pk>/update/", NoteUpdateView.as_view(), name="note_update"),
     path("create/", NoteCreateView.as_view(), name="note_create"),
 ]
--- example_project/simple_app/views.py
+++ example_project/simple_app/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.http import HttpResponse, HttpRequest
+from django.urls import reverse_lazy, reverse
+from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView, DetailView
+
+from .forms import NoteForm
 from .models import Note
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class NotesIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+        }
+        return render(request, 'simple_app/note_index.html', context=context)
 
+
+class NoteDetailView(DetailView):
+    template_name = "simple_app/note_details.html"
+    model = Note
+    context_object_name = "note"
 
 class NotesListView(ListView):
     template_name = "simple_app/notes_list.html"
@@ -18,3 +29,12 @@ class NoteCreateView(CreateView):
     model = Note
     fields = "label", "text"
     success_url = reverse_lazy("simple_app:notes_list")
+
+
+class NoteUpdateView(UpdateView):
+    model = Note
+    template_name = "simple_app/note_update_form.html"
+    form_class = NoteForm
+
+    def get_success_url(self):
+        return reverse("simple_app:notes_list")
