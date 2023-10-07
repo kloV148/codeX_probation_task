2130ca4c233e28dfb74f61e4121a47fc5d8f10e7
IvanS <mainpostskvorcov@gmail.com>
2023-10-07 23:40:02 +0300

    added http input in telegraf

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'example_project.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django_prometheus.db.backends.postgresql',
+    'ENGINE': 'django.db.backends.postgresql',
     'NAME': os.environ.get('POSTGRES_NAME'),
     'USER': os.environ.get('POSTGRES_USER'),
     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-    'HOST': 'db',
-    'PORT': 5432,
+    'HOST': os.environ.get('POSTGRES_HOST'),
+    'PORT': os.environ.get('POSTGRES_PORT')
 }
 }
 
